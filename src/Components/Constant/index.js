fdea9c3e2bed971a94f34962b60d52d64c727f5b
Koshti Sagar <107100610+sagar7778@users.noreply.github.com>
2024-08-30 11:58:54 +0530

    all page add

---
+++ src/Components/Constant/index.js
@@ -0,0 +1,32 @@
+export const navLinksdata = [
+  {
+    _id: 1001,
+    title: "Home",
+    link: "home",
+  },
+  {
+    _id: 1002,
+    title: "Features",
+    link: "features",
+  },
+  {
+    _id: 1003,
+    title: "Portfolio",
+    link: "portfolio",
+  },
+  {
+    _id: 1004,
+    title: "Resume",
+    link: "resume",
+  },
+  {
+    _id: 1005,
+    title: "Blog",
+    link: "blog",
+  },
+  {
+    _id: 1006,
+    title: "Contact",
+    link: "contact",
+  },
+];
