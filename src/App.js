67931441cd9d45546df4902bdfc34e6776cdf4b5
Koshti Sagar <107100610+sagar7778@users.noreply.github.com>
2024-08-30 11:59:19 +0530

    all page add

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import "./App.css";
 import Header from "./Components/Header";
+// import Home from "./Components/Home"
+import Home from "./Components/Home2";
+import Features from "./Components/Features";
+import Portfolio from "./Components/Portfolio";
+import Resume from "./Components/Resume";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+import Blog from "./Components/Blog";
 
 function App() {
   return (
     <div>
       <Header />
+      <Home />
+      <Features />
+      <Portfolio />
+      <Resume />
+      <Blog />
+      <Contact />
+      <Footer />
     </div>
   );
 }
